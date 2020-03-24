2c2b9e7be2caca5063eb7c94a31d084f5c5a55e7
Jack Thomson <thomsonjack41@gmail.com>
2020-03-23 23:22:53 -0400

    First commit of working extension ⚡

---
+++ options/options.js
@@ -0,0 +1,108 @@
+// There's gotta be a smarter way to do this...
+
+
+
+// var left = document.getElementById("left");
+// var featured = document.getElementById("featured");
+// var hotnetwork = document.getElementById("hotnetwork");
+// var linked = document.getElementById("linked");
+// var related = document.getElementById("related");
+// var newsletter = document.getElementById("newsletter");
+// var feed = document.getElementById("feed");
+// var right = document.getElementById("right");
+// var jobs = document.getElementById("jobs");
+
+function getCheckElements() {
+
+    var storedElements = {
+        left : document.getElementById("left"),
+        featured : document.getElementById("featured"),
+        hotnetwork : document.getElementById("hotnetwork"),
+        linked : document.getElementById("linked"),
+        related : document.getElementById("related"),
+        newsletter : document.getElementById("newsletter"),
+        feed : document.getElementById("feed"),
+        right : document.getElementById("right"),
+        jobs : document.getElementById("jobs")
+    };
+
+    return storedElements;
+}
+
+function getCheckElementStatus() {
+
+    var varStoredChecks = {
+
+        leftstored : document.getElementById("left").checked,
+        featuredstored : document.getElementById("featured").checked,
+        hotnetworkstored : document.getElementById("hotnetwork").checked,
+        linkedstored : document.getElementById("linked").checked,
+        relatedstored : document.getElementById("related").checked,
+        newsletterstored : document.getElementById("newsletter").checked,
+        feedstored : document.getElementById("feed").checked,
+        rightstored : document.getElementById("right").checked,
+        jobsstored : document.getElementById("jobs").checked
+    
+    };
+
+    return varStoredChecks;
+}
+
+
+function saveOptions(e) {
+    e.preventDefault();
+    myvar = getCheckElementStatus();
+
+    browser.storage.sync.set({
+        storedValues : myvar
+    });
+  }
+  
+  function restoreOptions() {
+
+    function setCurrentChoice(result) {
+
+        checkObjects = getCheckElements();
+
+        checkObjects.left.checked = result.storedValues.leftstored;
+        checkObjects.featured.checked = result.storedValues.featuredstored;
+        checkObjects.hotnetwork.checked = result.storedValues.hotnetworkstored;
+        checkObjects.linked.checked = result.storedValues.linkedstored;
+        checkObjects.related.checked = result.storedValues.relatedstored;
+        checkObjects.newsletter.checked = result.storedValues.newsletterstored;
+        checkObjects.feed.checked = result.storedValues.feedstored;
+        checkObjects.right.checked = result.storedValues.rightstored;
+        checkObjects.jobs.checked = result.storedValues.jobsstored;
+
+    }
+  
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    let gettingstoredValues = browser.storage.sync.get("storedValues");
+    gettingstoredValues.then(setCurrentChoice, onError);
+
+
+  }
+  
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+// var checkElements = getCheckElements();
+// console.log(checkElements);
+// for (var checks in checkElements) {
+//     console.log(checks);
+//     console.log(Object.keys(checks))
+//     debugger;
+//     checks.addEventListener("change", saveOptions);
+// }
+  left.addEventListener("change", saveOptions);
+  featured.addEventListener("change",saveOptions);
+  hotnetwork.addEventListener("change",saveOptions);
+  linked.addEventListener("change",saveOptions);
+  related.addEventListener("change",saveOptions);
+  newsletter.addEventListener("change",saveOptions);
+  feed.addEventListener("change",saveOptions);
+  right.addEventListener("change",saveOptions);
+  jobs.addEventListener("change", saveOptions);
+
---
+++ stackblocker.js
@@ -0,0 +1,66 @@
+function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+  
+  function onGot(item) {
+
+    console.log(item);
+
+    var leftside = document.getElementById("left-sidebar");
+    if (item.storedValues.leftstored && leftside != null) {
+        console.log("left!");
+        leftside.parentNode.removeChild(leftside);
+    }
+
+    var hot_network = document.getElementById("hot-network-questions");
+    if (item.storedValues.hotnetworkstored && hot_network != null) {
+        console.log("hot!");
+        hot_network.parentNode.removeChild(hot_network);
+    }
+
+    var newsletter = document.getElementById("newsletter-ad");
+    if (item.storedValues.newsletterstored && newsletter != null) {
+        console.log("news!");
+        newsletter.parentNode.removeChild(newsletter);
+    }
+
+    var feed = document.getElementById("feed-link");
+    if (item.storedValues.feedstored && feed != null) {
+        console.log("feed!");
+        feed.parentNode.removeChild(feed);
+    }
+
+    var jobs = document.getElementById("hireme");
+    if (item.storedValues.jobsstored && jobs != null) {
+        console.log("jobs!");
+        jobs.parentNode.removeChild(jobs);
+    }
+
+    var linked = document.querySelectorAll('.sidebar-linked');
+    if (item.storedValues.linkedstored && linked.length) {
+        console.log("linked!");
+        linked[0].parentNode.removeChild(linked[0]);
+    }
+
+    var related = document.querySelectorAll('.sidebar-related');
+    if (item.storedValues.relatedstored && related.length){
+        console.log("related!");
+        related[0].parentNode.removeChild(related[0]);
+    }
+
+    var featured = document.querySelectorAll('.s-sidebarwidget__yellow');
+    if (item.storedValues.featuredstored && featured.length) {
+        console.log("blog!");
+        featured[0].parentNode.removeChild(featured[0]);
+    }
+
+    var right = document.getElementById("sidebar");
+    if (item.storedValues.rightstored && right != null) {
+        console.log("feed!");
+        right.parentNode.removeChild(right);
+    }
+
+  }
+  
+  let getting = browser.storage.sync.get("storedValues");
+  getting.then(onGot, onError);
