f644c687cd7bbbc736c23d4c493f5f477a05eab4
Jack Thomson <thomsonjack41@gmail.com>
2020-03-24 23:29:55 -0400

    Move to 1.0 release with cleaned up code

--- options/options.js
+++ options/options.js
@@ -1,15 +1,20 @@
-var left = document.getElementById("left");
+/*
+This file contains the javascript for the options menu within
+stackblocker.
+*/
+
+// The current html check elements
 var featured = document.getElementById("featured");
 var hotnetwork = document.getElementById("hotnetwork");
 var linked = document.getElementById("linked");
 var related = document.getElementById("related");
 var newsletter = document.getElementById("newsletter");
 var feed = document.getElementById("feed");
-var right = document.getElementById("right");
 var jobs = document.getElementById("jobs");
+var left = document.getElementById("left");
 
+// Get the current html elements for the check
 function getCheckElements() {
-
     var storedElements = {
         left: document.getElementById("left"),
         featured: document.getElementById("featured"),
@@ -18,33 +23,29 @@ function getCheckElements() {
         related: document.getElementById("related"),
         newsletter: document.getElementById("newsletter"),
         feed: document.getElementById("feed"),
-        right: document.getElementById("right"),
         jobs: document.getElementById("jobs")
     };
 
     return storedElements;
 }
 
+// Get the current value of check marks on options.html
 function getCheckElementStatus() {
-
     var varStoredChecks = {
-
-        leftstored: document.getElementById("left").checked,
-        featuredstored: document.getElementById("featured").checked,
-        hotnetworkstored: document.getElementById("hotnetwork").checked,
-        linkedstored: document.getElementById("linked").checked,
-        relatedstored: document.getElementById("related").checked,
-        newsletterstored: document.getElementById("newsletter").checked,
-        feedstored: document.getElementById("feed").checked,
-        rightstored: document.getElementById("right").checked,
-        jobsstored: document.getElementById("jobs").checked
-
+        featuredChecked: document.getElementById("featured").checked,
+        hotChecked: document.getElementById("hotnetwork").checked,
+        linkedChecked: document.getElementById("linked").checked,
+        relatedChecked: document.getElementById("related").checked,
+        newsChecked: document.getElementById("newsletter").checked,
+        feedChecked: document.getElementById("feed").checked,
+        jobsChecked: document.getElementById("jobs").checked,
+        leftChecked: document.getElementById("left").checked
     };
 
     return varStoredChecks;
 }
 
-
+// Save checked options to storage
 function saveOptions(e) {
     e.preventDefault();
     myvar = getCheckElementStatus();
@@ -54,51 +55,40 @@ function saveOptions(e) {
     });
 }
 
+// Set the checked options to visibly match their stored values
 function restoreOptions() {
 
     function setCurrentChoice(result) {
-
         checkObjects = getCheckElements();
-
-        checkObjects.left.checked = result.storedValues.leftstored;
-        checkObjects.featured.checked = result.storedValues.featuredstored;
-        checkObjects.hotnetwork.checked = result.storedValues.hotnetworkstored;
-        checkObjects.linked.checked = result.storedValues.linkedstored;
-        checkObjects.related.checked = result.storedValues.relatedstored;
-        checkObjects.newsletter.checked = result.storedValues.newsletterstored;
-        checkObjects.feed.checked = result.storedValues.feedstored;
-        checkObjects.right.checked = result.storedValues.rightstored;
-        checkObjects.jobs.checked = result.storedValues.jobsstored;
+        // Set the checks to their stored values
+        checkObjects.featured.checked = result.storedValues.featuredChecked;
+        checkObjects.hotnetwork.checked = result.storedValues.hotChecked;
+        checkObjects.linked.checked = result.storedValues.linkedChecked;
+        checkObjects.related.checked = result.storedValues.relatedChecked;
+        checkObjects.newsletter.checked = result.storedValues.newsChecked;
+        checkObjects.feed.checked = result.storedValues.feedChecked;
+        checkObjects.jobs.checked = result.storedValues.jobsChecked;
+        checkObjects.left.checked = result.storedValues.leftChecked;
 
     }
 
+    // Log any errors
     function onError(error) {
         console.log(`Error: ${error}`);
     }
 
-    let gettingstoredValues = browser.storage.sync.get("storedValues");
-    gettingstoredValues.then(setCurrentChoice, onError);
-
-
+    let currentCheckedValues = browser.storage.sync.get("storedValues");
+    currentCheckedValues.then(setCurrentChoice, onError);
 }
 
-
+// Reload options when opening the menu
 document.addEventListener("DOMContentLoaded", restoreOptions);
-// var checkElements = getCheckElements();
-// console.log(checkElements);
-// for (var checks in checkElements) {
-//     console.log(checks);
-//     console.log(Object.keys(checks))
-//     debugger;
-//     checks.addEventListener("change", saveOptions);
-// }
-left.addEventListener("change", saveOptions);
+// Save options whenever any box is checked
 featured.addEventListener("change", saveOptions);
 hotnetwork.addEventListener("change", saveOptions);
 linked.addEventListener("change", saveOptions);
 related.addEventListener("change", saveOptions);
 newsletter.addEventListener("change", saveOptions);
 feed.addEventListener("change", saveOptions);
-right.addEventListener("change", saveOptions);
 jobs.addEventListener("change", saveOptions);
-
+left.addEventListener("change", saveOptions);
--- stackblocker.js
+++ stackblocker.js
@@ -1,66 +1,60 @@
+/*
+This file contains the actual blocking elements of stackblocker.
+It simply removes the elements that are the specified types.
+*/
+
+// Log any errors
 function onError(error) {
     console.log(`Error: ${error}`);
 }
 
 function onGot(item) {
 
-    console.log(item);
-
+    // These return the elements themselves
     var leftside = document.getElementById("left-sidebar");
-    if (item.storedValues.leftstored && leftside != null) {
-        console.log("left!");
-        leftside.parentNode.removeChild(leftside);
+    if (item.storedValues.leftChecked && leftside != null) {
+        // Setting to hidden to the page not center
+        leftside.style.visibility = 'hidden';
     }
 
     var hot_network = document.getElementById("hot-network-questions");
-    if (item.storedValues.hotnetworkstored && hot_network != null) {
-        console.log("hot!");
+    if (item.storedValues.hotChecked && hot_network != null) {
         hot_network.parentNode.removeChild(hot_network);
     }
 
     var newsletter = document.getElementById("newsletter-ad");
-    if (item.storedValues.newsletterstored && newsletter != null) {
-        console.log("news!");
+    if (item.storedValues.newsChecked && newsletter != null) {
         newsletter.parentNode.removeChild(newsletter);
     }
 
     var feed = document.getElementById("feed-link");
-    if (item.storedValues.feedstored && feed != null) {
-        console.log("feed!");
+    if (item.storedValues.feedChecked && feed != null) {
         feed.parentNode.removeChild(feed);
     }
 
     var jobs = document.getElementById("hireme");
-    if (item.storedValues.jobsstored && jobs != null) {
-        console.log("jobs!");
+    if (item.storedValues.jobsChecked && jobs != null) {
         jobs.parentNode.removeChild(jobs);
     }
 
+    // These return node lists
     var linked = document.querySelectorAll('.sidebar-linked');
-    if (item.storedValues.linkedstored && linked.length) {
-        console.log("linked!");
+    if (item.storedValues.linkedChecked && linked.length) {
         linked[0].parentNode.removeChild(linked[0]);
     }
 
     var related = document.querySelectorAll('.sidebar-related');
-    if (item.storedValues.relatedstored && related.length) {
-        console.log("related!");
+    if (item.storedValues.relatedChecked && related.length) {
         related[0].parentNode.removeChild(related[0]);
     }
 
     var featured = document.querySelectorAll('.s-sidebarwidget__yellow');
-    if (item.storedValues.featuredstored && featured.length) {
-        console.log("blog!");
+    if (item.storedValues.featuredChecked && featured.length) {
         featured[0].parentNode.removeChild(featured[0]);
-    }
-
-    var right = document.getElementById("sidebar");
-    if (item.storedValues.rightstored && right != null) {
-        console.log("feed!");
-        right.parentNode.removeChild(right);
     }
 
 }
 
+// Get the stored values via lazy evaluation
 let getting = browser.storage.sync.get("storedValues");
 getting.then(onGot, onError);
